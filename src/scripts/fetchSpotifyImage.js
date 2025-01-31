50aa0ebaf3c9321ec4759b40560bcb40fa9d2f28
Camdyn Rasque <rasque.camdyn@gmail.com>
2025-01-30 21:20:15 -0800

    Fixed bug with writing files before subdirectory exists

--- src/scripts/fetchSpotifyImage.js
+++ src/scripts/fetchSpotifyImage.js
@@ -3,6 +3,11 @@ import path from 'node:path';
 
 export async function fetchSpotifyImage(url, filePath) {
   let assetPath = path.resolve(`./public/${filePath}`);
+
+  // Ensure the directory exists
+  let assetDir = path.dirname(assetPath);
+  fs.mkdirSync(assetDir, { recursive: true });
+
   if (fs.existsSync(assetPath)) return;
   else {
     let response = await fetch(url);
